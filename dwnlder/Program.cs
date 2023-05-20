dfbde5dc6c4ddd9d8f4b9f3778ea0ff0d5031481
Lucas Camacho <pessolilucas@gmail.com>
2023-05-20 10:49:24 -0300

    FIX renaming method LinkValidation to LinkAndArchiveTypeValidation to keep code clean

--- dwnlder/Program.cs
+++ dwnlder/Program.cs
@@ -5,38 +5,41 @@ using System.Threading;
 partial class Program
 {
 
-    public static void LinkValidation(ref String link, ref String archiveType)
+    public static string LinkHttpsFormatter(String insertedLink)
     {
+        string webPrefix = "htpps://";
+        insertedLink = webPrefix + insertedLink;
+        return insertedLink;
+    }
 
-        public static string LinkHttpsFormatter(String insertedLink)
-        {
-            string webPrefix = "htpps://";
-            insertedLink = webPrefix + insertedLink;
-            return insertedLink;
-        }
+    public static string ArchiveFormatter(String type)
+    {
+        char charToBeRemoved = '.';
+        int index = type.IndexOf(charToBeRemoved);
+        type = type.Remove(index, 1); //1 is the quantity of characters to be replaced
+        return type;
+    }
 
-        public static string ArchiveFormatter(String type)
-        {
-            char charToBeRemoved = '.';
-            int index = type.IndexOf(charToBeRemoved);
-            type = type.Remove(index, 1); //1 is the quantity of characters to be replaced
-            return type;
-
-        }
-
+    public static void LinkAndArchiveTypeValidation(ref String link, ref String archiveType)
+    {
+        //Formatting link
         if (!link.Contains("https://"))
         {
             link = LinkHttpsFormatter(link);
         }
+
         if (!link.Contains("."))
         {
             Console.WriteLine("did u forgot the .?");
+            Environment.Exit(1);
         }
         if (!link.Contains("www"))
         {
             Console.WriteLine("no www prefix found, exiting program...");
             Environment.Exit(1);
         }
+
+        //Formatting Archive
         if (archiveType.Contains("."))
         {
             archiveType = ArchiveFormatter(archiveType);
@@ -50,7 +53,7 @@ partial class Program
         Console.WriteLine("insert the archive type. Example: (txt,exe,zip... etc)");
         String archiveType = Console.ReadLine();
 
-        LinkValidation(ref link, ref archiveType);
+        LinkAndArchiveTypeValidation(ref link, ref archiveType);
 
         Console.WriteLine("-------------");
     }
