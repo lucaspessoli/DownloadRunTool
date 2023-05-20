48d7d3291eca431bb9fbc258f7e224b0addf2cf8
Lucas Camacho <pessolilucas@gmail.com>
2023-05-20 09:53:37 -0300

    STARTING REPO

---
+++ dwnlder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+
+
+partial class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("insert your download link: ");
+        String link = Console.ReadLine();
+        if (link.Contains("https://") && link.Contains(".com"))
+        {
+            Console.WriteLine("Link válido!");
+        }
+        else
+        {
+            Console.WriteLine("Link invalido!");
+        }
+    }
+}
