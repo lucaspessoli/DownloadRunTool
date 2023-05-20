2559d2c11de36e75cab89d75171daf3ffebefb6a
Lucas Camacho <pessolilucas@gmail.com>
2023-05-20 11:31:22 -0300

    IMPLEMENT better feedback for user, try&catch showing possible error messages

--- dwnlder/Program.cs
+++ dwnlder/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Net;
-
+using System.Linq.Expressions;
 
 partial class Program
 {
@@ -55,11 +55,11 @@ partial class Program
         Console.WriteLine("insert your download link: ");
         String link = Console.ReadLine();
 
-        Console.WriteLine("insert the archive type. Example: (txt,exe,zip... etc)");
-        String archiveType = Console.ReadLine();
-
         Console.WriteLine("name your archive: (do NOT use these characters: < > : ' / \\ | ? * or any other special chars");
         String archiveName = Console.ReadLine();
+
+        Console.WriteLine("insert the archive type. Example: (txt,exe,zip... etc)");
+        String archiveType = Console.ReadLine();
 
         Console.WriteLine("insert your directory (empty to C:\\)");
         String directory = Console.ReadLine();
@@ -74,8 +74,21 @@ partial class Program
 
         using (WebClient client = new WebClient())
         {
-            client.DownloadFile(link, fileDirectory);
+            try
+            {
+                Console.WriteLine("Downloading...");
+                client.DownloadFile(link, fileDirectory);
+            }catch(Exception e)
+            {
+                Console.WriteLine("error downloading your file, error: " + e.Message);
+                Console.WriteLine("Possible Reasons: no adm privileges found, try open the software as a administrator, your download host is offline or invalid directory");
+                Console.WriteLine("Exiting in 5 seconds...");
+                Thread.Sleep(5000);
+                Environment.Exit(1);
+            }
         }
+        Console.WriteLine("Thanks for using Downloader Tool! Exiting in 5 seconds...");
+        Thread.Sleep(5000);
 
         Console.WriteLine("-------------");
     }
